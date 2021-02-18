a65a96adee647c96c826566c31164ac0b9d8e199
Piero Proietti <piero.proietti@gmail.com>
2021-02-18 17:16:04 +0100

    working...

--- classes/utils.py
+++ classes/utils.py
@@ -14,7 +14,18 @@ class Utils:
    
 
    def vmlinuz(self):
-      bashCommand ="cat /proc/cmdline|/usr/bin/cut -f1 -d ' ' |/usr/bin/cut -f2 -d '='"
-      process = subprocess.Popen(bashCommand.split(), shell=False, stdout=subprocess.PIPE)
-      output = process.stdout.read() 
-      return output
+      bashCmd1 = "cat /proc/cmdline"
+      bashCmd2 = "/usr/bin/cut -f1 -d ' '"
+      bashCmd3 = "/usr/bin/cut -f2 -d ="
+
+      #bashCommand ="cat /proc/cmdline|/usr/bin/cut -f1 -d ' ' |/usr/bin/cut -f2 -d '='"
+      output1 = subprocess.Popen(bashCmd1.split(), stdout=subprocess.PIPE)
+      print('output1')
+      print(output1.stdout)
+      print('output2')
+      output2 = subprocess.Popen(bashCmd2.split(), stdout=subprocess.PIPE, stdin=output1.stdout)
+      print('output3')
+      print(output2.stdout)
+      output3 = subprocess.Popen(bashCmd3.split(), stdout=subprocess.PIPE, stdin=output2.stdout) 
+      print(output3.stdout)
+      return output3
